41345c00533b64d14ddf493f98df8a18f349c5ae
unknown <jordindlrosa@gmail.com>
2019-11-18 21:50:58 -0500

    Movement with Jump and Crouch

--- Documents/Game Design/Movement/Assets/Scripts/PlayerMove.cs
+++ Documents/Game Design/Movement/Assets/Scripts/PlayerMove.cs
@@ -2,33 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour{
-
+public class PlayerMove : MonoBehaviour
+{
     public CharacterController2D controller;
 
     public float runSpeed = 40f;
-    
+    public float sprintSpeed = 70f;
     float horizontalMove = 0f;
 
+    bool jump = false;
+    bool crouch = false;
+    bool sprint = false;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+
+        //Moves the Character
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        //Makes the character Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        //Makes the character Crouch
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
 
+        //Makes the Character Run
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            sprint = true;
 
-
+        } else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            sprint = false;
+        }
     }
 
-  void FixedUpdate()
+    void FixedUpdate()
     {
         //Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+       
+
+        
     }
 }
